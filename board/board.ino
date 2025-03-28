0c80255f66b7dbcc42d8a915b4a71d223019c5b2
00glitched <conaeze3@gmail.com>
2025-03-28 00:58:14 +0000

    Communication: Commands

--- board/board.ino
+++ board/board.ino
@@ -1,15 +1,16 @@
 const uint8_t bufferSerial = 32;
 uint8_t readSerial[bufferSerial];
-
- struct channel
-  {
+uint8_t FRAMES=15;
+// struct channel
+//  {
+    
     uint8_t PIN;    // Input pin
     uint8_t MODE;   // Mode (osc/logic)
     uint8_t READ;   // 
     uint8_t DELAY;  // Delay (1= 100uSec)
     uint8_t SIZE;
     uint8_t NBYTE;
-  };  
+//  };
 
 
 uint8_t i=0;
@@ -17,7 +18,7 @@ uint8_t i=0;
 uint16_t ADCread[256];
 uint8_t  toSend[512];
 
-uint32_t TIMER = Delay;
+uint32_t TIMER = DELAY;
 
 void setup()
   {
@@ -28,24 +29,26 @@ void serialEvent() // The CLI of board
   {
     if (Serial.available())
       {
-        uint8_t readByte = Serial.peek();
-
+        //uint8_t readByte = Serial.peek();
+        uint8_t readByte = Serial.read();
         switch (readByte)
           {
+            // Run
+            case 'R': READ = true; Serial.println("RUN!");  break;
+            // stop
+            case 'S': READ = false; Serial.println("STOP!"); break;
+            
             // Delay by sample
-            case 'T': Delay = Serial.parseInt(); serial.Print("Sampling time changed to DELAY=");serial.Println(Delay); break;
+            case 'T': DELAY = Serial.parseInt(); Serial.print("Sampling time changed to DELAY=");Serial.print(DELAY);Serial.println("ms"); break;
             
             // Number of samples
-            case 'N': Nsamp = Serial.parseInt(); serial.Print("Sampling size changed to SIZE=");serial.Println(Nsamp); break;
+            case 'N': SIZE = Serial.parseInt(); Serial.print("Sampling size changed to SIZE=");Serial.print(SIZE);Serial.println("samples"); break;
 
             // Number of bytes
-            case 'B': Nbyte = Serial.parseInt(); serial.Print("Sampling byte changed to NBYTE=");serial.Println(Nbyte); break;
+            case 'B': NBYTE = Serial.parseInt(); Serial.print("Sampling byte changed to NBYTE=");Serial.print(NBYTE);Serial.println("bytes"); break;
 
-            // Run
-            case 'R': Read = true;  break;
-            // stop
-            case 'S': Read = false; break;
-
+            // Number of frames
+            case 'F': FRAMES = Serial.parseInt(); Serial.print("Sampling frames changed to FRAMES=");Serial.print(FRAMES);Serial.println("fps"); break;
             
           }
       }
@@ -53,26 +56,33 @@ void serialEvent() // The CLI of board
 
 void loop()
   {
-    if (Read == true)
+    if (MODE == 1)
       {
-        // Read routine
-        uint32_t TIME = micros();
-        if (TIME > TIMER)
+        if (READ == true)
           {
-            // Read ADC
-            ADCread[i] = analogRead(A0);
-            
-            // Convert ADC vale to bytes to send
-            if (Nbyte>1){ toSend[i*2] = ADCread[i]>>2; toSend[i*2+1] = ADCread[i] & 0b0000000000000011; }
-            else { toSend[i] = ADCread[i]>>2; }
-            
-            // Time functions to generate wave
-            i++; if (i > Nsamp) { i=0; }
-            TIMER += Delay;
+            // Read routine
+            uint32_t TIME = micros();
+            if (TIME > TIMER)
+              {
+                // Read ADC
+                ADCread[i] = analogRead(A0);
+                
+                // Convert ADC vale to bytes to send
+                if (NBYTE>1){ toSend[i*2] = ADCread[i]>>2; toSend[i*2+1] = ADCread[i] & 0b0000000000000011; }
+                else { toSend[i] = ADCread[i]>>2; }
+                
+                // Time functions to generate wave
+                i++; if (i > SIZE) { i=0; }
+                TIMER += DELAY;
+              }
           }
-      }
-    else
-      {
-        delay(10); // Do nothing
+        else if (true)
+          {
+          
+          }
+        else
+          {
+            delay(10); // Do nothing
+          }
       }
   }
