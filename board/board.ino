17f82488a119c67f87ebfd4e6cf590462d055411
00glitched <conaeze3@gmail.com>
2025-03-29 17:50:57 +0000

    Board main: CLI + sampling

--- board/board.ino
+++ board/board.ino
@@ -1,12 +1,17 @@
-const uint8_t bufferSerial = 32;
-uint8_t readSerial[bufferSerial];
-uint8_t FRAMES=15;
+uint8_t FRAMES=15; uint16_t DELAYFRAMES=15;
+
+//         -1 bitshift   5fps    6fps   7fps
+//                      10fps   12fps  15fps
+//         +1 bitshift  20fps   25fps  30fps
+//         +2 bitshift  40fps   50fps  60fps
+uint32_t delays_frames[3]={1000000,800000,666666}; uint8_t n=0; uint8_t k=0;
+
 // struct channel
 //  {
     
     uint8_t PIN;    // Input pin
     uint8_t MODE;   // Mode (osc/logic)
-    uint8_t READ;   // 
+    uint8_t READ;   //    
     uint8_t DELAY;  // Delay (1= 100uSec)
     uint8_t SIZE;
     uint8_t NBYTE;
@@ -18,7 +23,8 @@ uint8_t i=0;
 uint16_t ADCread[256];
 uint8_t  toSend[512];
 
-uint32_t TIMER = DELAY;
+uint32_t TIMERANALOG = DELAY;
+uint32_t TIMERFRAMES = DELAYFRAMES;
 
 void setup()
   {
@@ -62,18 +68,26 @@ void loop()
           {
             // Read routine
             uint32_t TIME = micros();
-            if (TIME > TIMER)
+            if (TIME > TIMERANALOG)
               {
                 // Read ADC
                 ADCread[i] = analogRead(A0);
                 
                 // Convert ADC vale to bytes to send
-                if (NBYTE>1){ toSend[i*2] = ADCread[i]>>2; toSend[i*2+1] = ADCread[i] & 0b0000000000000011; }
-                else { toSend[i] = ADCread[i]>>2; }
+                if (NBYTE>1)
+                  {
+                    toSend[i*2] = ADCread[i]>>2; toSend[i*2+1] = ADCread[i] & 0b0000000000000011; // two-bytes
+                  }
+                else { toSend[i] = ADCread[i]>>2; } // one-byte
                 
                 // Time functions to generate wave
                 i++; if (i > SIZE) { i=0; }
-                TIMER += DELAY;
+                TIMERANALOG += DELAY;
+              }
+            if (TIME > TIMERFRAMES)
+              {
+                // frames-per-second with bitshift
+                TIMERFRAMES += delays_frames[k] >> n;
               }
           }
         else if (true)
